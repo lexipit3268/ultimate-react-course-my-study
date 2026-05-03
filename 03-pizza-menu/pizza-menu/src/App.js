09cd0b4f0381d4a0b618d94809bc4bdb2a119165
Nguyen Loc <locdedoda@gmail.com>
2026-05-03 16:35:51 +0700

    finish small pizza app

--- 03-pizza-menu/pizza-menu/src/App.js
+++ 03-pizza-menu/pizza-menu/src/App.js
@@ -69,12 +69,12 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
         {/* <p>{pizzaObj.soldOut}</p> */}
       </div>
     </li>
